3f1d7f6b1ab4f74179fd9fbc1d4b1941716d8b5d
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-10-31 14:06:04 +0300

    add tests 1 2 4

---
+++ test3/Test3.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.HashMap;
+
+public class Test3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String t = br.readLine();
+        String v = br.readLine();
+
+        BufferedReader valuesReader = new BufferedReader(new FileReader(v));
+
+        HashMap<Long, String> values = new HashMap<>();
+
+        while (valuesReader.ready()) {              // разбиваем values.json на  хеш-таблицу
+            String s = valuesReader.readLine().replaceAll("\\D", "");
+            if (s.length() > 0) {
+                long id = Long.parseLong(s);
+                String[] str = valuesReader.readLine().split(" ");
+                String val = str[str.length - 1];
+                values.put(id, val);
+            }
+        }
+
+        BufferedReader testsReader = new BufferedReader(new FileReader(t));
+        BufferedWriter reportWriter = new BufferedWriter(new FileWriter("report.json"));
+
+        while (testsReader.ready()) {
+            // принцип такой, берем строку, ищем есть ли в ней id, ищем id в хеш-таблице форматируем нужную строку,
+            // записываем в репорт. Все остальное просто записываем без изменений.
+
+            String s = testsReader.readLine();
+
+            if (s.contains("\"id\":")) {
+                reportWriter.write(s);
+                reportWriter.newLine();
+                long id = Integer.parseInt(s.replaceAll("\\D", ""));
+
+                if (!values.containsKey(id)) { // есть ли тест в тестовых списках, если нет то ищем дальше
+                    continue;
+                }
+
+                reportWriter.write(testsReader.readLine()); // пропускаем строку с описанием
+                reportWriter.newLine();
+
+                String str = testsReader.readLine(); // далее задаем строке value нужное зачение
+
+                boolean mark = str.charAt(str.length() - 1) == ',';
+                String[] val = str.split(" ");
+                if (mark) {
+                    val[val.length - 1] = values.get(id) + ",";
+                } else {
+                    val[val.length - 1] = values.get(id);
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                for (String str1 : val) {
+                    stringBuilder.append(str1).append(" ");
+                }
+
+                reportWriter.write(String.valueOf(stringBuilder));
+                reportWriter.newLine();
+            } else {
+                reportWriter.write(s);
+                reportWriter.newLine();
+            }
+        }
+
+        br.close();
+        valuesReader.close();
+        testsReader.close();
+        reportWriter.close();
+    }
+}
