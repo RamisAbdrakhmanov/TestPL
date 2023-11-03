e6c5d60a3c0cc9a09e3be675dcc6e5156e4140d5
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-11-03 17:49:34 +0300

    update tests all + add manifest

--- test1/Test1.java
+++ test1/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int step = Integer.parseInt(args[1]);
+        int step = Integer.parseInt(args[1]) - 1;
 
         int numb = 1;
         System.out.print(1);
